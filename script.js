7bfeabbc2ecce0a507086bab7f3844a92d8c7b14
kenny <kennedy_ken99@hotmail.com>
2025-11-23 18:02:10 -0300

    add o item a lista

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
 const newItem = document.getElementById("newItem") // capturando o novo item digitado pelo usuario
-const addItem = document.getElementById("addItem") // capturando o botao
 let listaTarefas = document.getElementById("list") // capturando a lista (ul)
-const labels = document.createElement("label") // criando o label
-labels.classList.add("checkbox-label") // adicionando classe ao label
-const butonLixeira = document.createElement("button") // capturando o botao lixeira
-butonLixeira.classList.add("lixeira") // adicionando classe ao botao lixeira
-const inputOculto = document.createElement("input") // criando o input
-inputOculto.classList.add("check-oculto") // adicionando classe ao input
-inputOculto.type = "checkbox" // adicionando o atributo type("checkbox")
-const spanCustomizado = document.createElement("span") // capturando o span customizado
-spanCustomizado.classList.add("check-customizado") // adicionando classe ao span
-let spanNewItem = document.createElement("span") // capturando o span do item
-
-
-
 const alert = document.getElementById("alert") // capturando o alert de item removido da lista
 
 
-let item = newItem.value.trim() // capturando o valor sem espaços em branco
-
-/*
-newItem.addEventListener("input", (e) => {
-  if (item == "") {
-
-  }
-  console.log(item)
-})
-*/
-
 // função para adicionar um item (li) na lista (ul)
 function adicionarTarefa() {
-  // document.getElementById("").textContent = item
+  let item = newItem.value.trim() // capturando o valor sem espaços em branco
+  
+  // Garante que não adicionaremos uma tarefa vazia
+  if (item === "") {
+    alert("Por favor, digite um nome para a tarefa.")
+    return
+  }
+
+  // 1. Criando o li
   let novaTarefa = document.createElement("li") // cria o item (li)
-  listaTarefas.appendChild(novaTarefa) // adiciona o item na lista
+
+  // 2. criando os elementos filhos da li
+  const labels = document.createElement("label") // criando o label
+  labels.classList.add("checkbox-label") // adicionando classe ao label
+
+  const inputOculto = document.createElement("input") // criando o input
+  inputOculto.type = "checkbox" // adicionando o atributo type("checkbox")
+  inputOculto.classList.add("check-oculto") // adicionando classe ao input
+  
+  const spanCustomizado = document.createElement("span") // capturando o span customizado
+  spanCustomizado.classList.add("check-customizado") // adicionando classe ao span
+
+  let spanNewItem = document.createElement("span") // capturando o span do item
+  
+  const butonLixeira = document.createElement("button") // capturando o botao lixeira
+  butonLixeira.classList.add("lixeira") // adicionando classe ao botao lixeira
+
+  // 3 montando a estrutura 
+  
+  // 3.1 adicionando os elementos filhos da label
   labels.append(inputOculto, spanCustomizado, spanNewItem)
+
+  // 3.2 adicionando os elementos filhos da li
   novaTarefa.append(labels, butonLixeira) // adiciona o label na lista
-  
-  
-  
-  spanCustomizado.textContent = item
-  console.log(spanCustomizado)
-  
+
+  // 4 inserção do texto do usuario
+  spanNewItem.textContent = item // adicionando o item no span
+
+  // 5. Adiciona o LI completo à UL (Lista de Tarefas) 
+  listaTarefas.appendChild(novaTarefa) // adiciona o item na lista
+    
+  // 6. Limpa o input
   newItem.value = "" // limpa o input após adicionar algum item.
 }
