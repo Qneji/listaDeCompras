3b1c0945eb8d9259db58a9df283643af658e945a
kenny <kennedy_ken99@hotmail.com>
2025-11-27 22:41:12 -0300

    criando o array e sua lógica

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const newItem = document.getElementById("newItem") // capturando o novo item digitado pelo usuario
-const listaTarefas = document.getElementById("list") // capturando a lista (ul)
 const alert = document.getElementById("alert") // capturando o alert de item removido da lista
 const menssagemFooter = document.getElementById("menssagem")
 const imgSpan = document.getElementById("img-span")
 
+let ListaMercado = ["Pão de forma", "Café preto", "Suco de laranja", "Bolacha"]
 
 // função para adicionar um item (li) na lista (ul)
 function adicionarTarefa() {
@@ -22,41 +22,38 @@ function adicionarTarefa() {
     imgSpan.src = "./assets/icons/opcoes.svg"
     imgSpan.style.width = "1.3rem"
     imgSpan.style.height = "1.3rem"
-    
-    // 1. Criando o li
-    let novaTarefa = document.createElement("li") // cria o item (li)
-  
-    // 2. criando os elementos filhos da li
-    const labels = document.createElement("label") // criando o label
-    labels.classList.add("checkbox-label") // adicionando classe ao label
-  
-    const inputOculto = document.createElement("input") // criando o input
-    inputOculto.type = "checkbox" // adicionando o atributo type("checkbox")
-    inputOculto.classList.add("check-oculto") // adicionando classe ao input
-    
-    const spanCustomizado = document.createElement("span") // capturando o span customizado
-    spanCustomizado.classList.add("check-customizado") // adicionando classe ao span
-  
-    let spanNewItem = document.createElement("span") // capturando o span do item
-    
-    const butonLixeira = document.createElement("button") // capturando o botao lixeira
-    butonLixeira.classList.add("lixeira") // adicionando classe ao botao lixeira
-  
-    // 3 montando a estrutura 
-    
-    // 3.1 adicionando os elementos filhos da label
-    labels.append(inputOculto, spanCustomizado, spanNewItem)
-  
-    // 3.2 adicionando os elementos filhos da li
-    novaTarefa.append(labels, butonLixeira) // adiciona o label na lista
-  
-    // 4 inserção do texto do usuario
-    spanNewItem.textContent = item // adicionando o item no span
-  
-    // 5. Adiciona o LI completo à UL (Lista de Tarefas) 
-    listaTarefas.appendChild(novaTarefa) // adiciona o item na lista
+
+    ListaMercado.push(item)
+    renderizarTarefas()
   }
     
   // 6. Limpa o input
   newItem.value = "" // limpa o input após adicionar algum item.
 }
+
+function renderizarTarefas() {
+  const listaTarefas = document.getElementById("list")
+  listaTarefas.innerHTML = "";
+
+
+  for (i = 0; i < ListaMercado.length; i++) {
+    
+    let novaTarefa = document.createElement("li")
+    const labels = document.createElement("label")
+    labels.classList.add("checkbox-label")
+    const inputOculto = document.createElement("input")
+    inputOculto.type = "checkbox"
+    inputOculto.classList.add("check-oculto")
+    const spanCustomizado = document.createElement("span")
+    spanCustomizado.classList.add("check-customizado")
+    let spanNewItem = document.createElement("span")
+    const butonLixeira = document.createElement("button")
+    butonLixeira.classList.add("lixeira")
+    labels.append(inputOculto, spanCustomizado, spanNewItem)
+    novaTarefa.append(labels, butonLixeira)
+    spanNewItem.textContent = ListaMercado[i] // adicionando o item na lista
+    listaTarefas.appendChild(novaTarefa)
+  }
+}
+
+renderizarTarefas() // chamando a função para renderizar as tarefas
